6582e9990dd9f99a30b405f2d0c89f31ed4e050b
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-12-11 22:33:09 -0600

    Remove unused image URL from portfolio data

--- src/content_option.js
+++ src/content_option.js
@@ -71,7 +71,7 @@ const services = [
 
 
 const dataportfolio = [{
-  img: "https://picsum.photos/400/?grayscale",
+  img: "",
   description: "The wisdom of life consists in the elimination of non-essentials.",
   link: "#",
 },
